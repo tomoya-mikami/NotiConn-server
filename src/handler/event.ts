26eafb95035561833703cc7e908d66cc3b90c402
赤坂勝哉 <akasakatora1208@gmail.com>
2019-05-01 15:00:07 +0900

    del: dist

--- src/handler/event.ts
+++ src/handler/event.ts
@@ -3,9 +3,12 @@ import rp = require("request-promise");
 const router = Express.Router();
 
 router.get("/", async (req: Express.Request, res: Express.Response) => {
-  const baseUrl: string = "https://connpass.com/api/v1/event/?count=100";
+  const baseUrl: string = "https://connpass.com/api/v1/event";
   const options = {
     uri: baseUrl,
+    qs: {
+      count: 100
+    },
     json: true
   };
   rp(options)
